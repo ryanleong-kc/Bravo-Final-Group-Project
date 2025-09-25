b77ced19cd5b6acb51ebce6dbf0ab2f5ea60ff10
HazriqZalfar <binhafizal.1@osu.edu>
2025-09-25 21:10:58 +0800

    Just trying smth

--- BRAVO.py
+++ BRAVO.py
@@ -2,7 +2,18 @@ import random
 
 def fortune_teller():
   print("Welcome to Bravo's Fortune Teller!")
-  name = input("Please enter your name:")
-  print(f"Hello {name}, press Enter to receive your fortune
+  print("Please enter your :-")
+  name = input("Name :")
+  birthday = input("Birthday : ")
+  print(f"Hello {name}, press Enter to receive your fortune")
 
   fortunes = ["Expect good news in the near future."]
+
+
+print("""
+ -------------------------------
+|        FORTUNE TELLER         |
+|                               |
+ -------------------------------
+      """)
+fortune_teller()
